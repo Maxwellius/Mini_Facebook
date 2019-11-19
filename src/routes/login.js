1a61d25df801c5d88f5078b6ddd128e8a7b094af
Alexandre <hydrark07@gmail.com>
2019-11-19 15:28:13 +0100

    add routes inscription login.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -7,6 +7,11 @@ router.get('/', function(req, res){
    res.render('login/index.ejs', {root: process.cwd()});
 });
 
+router.get('/inscription', function(req, res){
+   console.log("GET Inscription page");
+   res.render('login/inscription.ejs', {root: process.cwd()});
+});
+
 
 router.post('/', function(req, res){
    var loginObject = new login();
