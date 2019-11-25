003e590fb5f8c17497476b1b2936e7923c35c532
Alexandre <hydrark07@gmail.com>
2019-11-25 15:46:15 +0100

    CSS update account

--- src/routes/account.js
+++ src/routes/account.js
@@ -2,23 +2,28 @@ var express = require('express');
 var AccountController = require('../controllers/account_controller');
 var router = express.Router();
 
-router.get('/login', function(req, res){
+router.get('/login', function (req, res) {
    console.log("GET Login page");
-   res.render('account/login.ejs', {root: process.cwd(), query: req.query});
+   res.render('account/login.ejs', { root: process.cwd(), query: req.query });
 });
 
-router.get('/inscription', function(req, res){
+router.get('/inscription', function (req, res) {
    console.log("GET Inscription page");
-   res.render('account/inscription.ejs', {root: process.cwd()});
+   res.render('account/inscription.ejs', { root: process.cwd() });
 });
 
-router.get('/modif_compte', function(req, res){
+router.get('/update', function (req, res) {
    console.log("GET Modification page");
-   res.render('login/modif_compte.ejs', {root: process.cwd()});
+   res.render('account/update.ejs', { root: process.cwd() });
+});
+
+router.post('/update', function (req, res) {
+   var account_controller = new AccountController(req, res);
+   // account_controller.update(...);
 });
 
 
-router.post('/login', function(req, res){
+router.post('/login', function (req, res) {
    var account_controller = new AccountController(req, res);
    account_controller.login(req.body.login, req.body.mdp);
 });
