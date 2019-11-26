8147e0e46fb2adffa39a3c44bda14d3ad22d8d81
Alexandre <hydrark07@gmail.com>
2019-11-26 12:22:12 +0100

    INscirption update account

--- src/routes/account.js
+++ src/routes/account.js
@@ -4,34 +4,12 @@ var router = express.Router();
 
 router.get('/login', function (req, res) {
    console.log("GET Login page");
-<<<<<<< HEAD
-   res.render('account/login.ejs', { root: process.cwd(), query: req.query });
-});
-
-router.get('/inscription', function (req, res) {
-   console.log("GET Inscription page");
-   res.render('account/inscription.ejs', { root: process.cwd() });
-});
-
-router.get('/update', function (req, res) {
-   console.log("GET Modification page");
-   res.render('account/update.ejs', { root: process.cwd() });
-});
-
-router.post('/update', function (req, res) {
-   var account_controller = new AccountController(req, res);
-   // account_controller.update(...);
-});
-
-
-router.post('/login', function (req, res) {
-   var account_controller = new AccountController(req, res);
-   account_controller.login(req.body.login, req.body.mdp);
-});
-=======
    if (req.session.user_id === -1 || req.session.user_id === undefined) {
       //L'utilisateur n'est pas encore connecté
-      res.render('account/login.ejs', { root: process.cwd(), query: req.query });
+      res.render('account/login.ejs', {
+         root: process.cwd(),
+         query: req.query
+      });
    } else {
       //L'utilisateur est connecté
       res.redirect('/')
@@ -47,14 +25,16 @@ router.get('/inscription', function (req, res) {
    console.log("GET Inscription page");
    if (req.session.user_id === -1 || req.session.user_id === undefined) {
       //L'utilisateur n'est pas encore connecté
-      res.render('account/inscription.ejs', { root: process.cwd() });
+      res.render('account/inscription.ejs', {
+         root: process.cwd()
+      });
    } else {
       //L'utilisateur est connecté
       res.redirect('/')
    }
 });
 
-router.post('/inscription', function(req, res){
+router.post('/inscription', function (req, res) {
    console.log("POST Inscription");
    var account_controller = new AccountController(req, res);
    var errorString = "";
@@ -62,14 +42,18 @@ router.post('/inscription', function(req, res){
    account_controller.inscription(req.body.login, req.body.mdp, req.body.nom, req.body.prenom);
 })
 
-router.get('/modif_compte', function (req, res) {
+router.get('/update', function (req, res) {
    console.log("GET Modification page");
-   res.render('login/modif_compte.ejs', { root: process.cwd() });
+   res.render('account/update.ejs', {
+      root: process.cwd()
+   });
+});
+
+router.post('/update', function (req, res) {
+   console.log("POST Modification page");
 });
 
 
-
->>>>>>> bd95ee651027db6e6133a89b09a04db754ae46b4
 
 //export this router to use in our index.js
 module.exports = router;
